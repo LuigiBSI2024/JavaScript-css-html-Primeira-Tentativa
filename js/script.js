243383c9aefea4697dbfa38df0e777174634d431
LuigiBSI2024 <laa10@aluno.ifnmg.edu.br>
2025-07-21 22:09:20 +0000

    Consertando e melhorando o .js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
+let interruptor = true; // Como o nome diz, serve para ser usado ocasionalmente assim como um interruptor real.
+const fechar = document.createElement("button");
+const container = document.createElement("div");
+
 function iniciarJogo(button){ //Button é o mesmo que botão em inglês.
     button.classList.add("ativo") //Repassando "ativo" como argumento para o método da propriedade presente na classList do button (aqui a classList está presente no button), neste caso .add(). Argumento é diferente de parâmetro, pois já sabemos que ele é.
     setTimeout(() => { // Declarando uma função anônima dentro do setTimeout, ela durará 2 segundos (=2000) antes de remover
@@ -7,17 +11,15 @@ function iniciarJogo(button){ //Button é o mesmo que botão em inglês.
 }
 
 function jogoExecutando() {
-    let finalizar_jogo = false;
-    let rodada = 0;
-
-    const container = document.createElement("div");
-    container.className = "jogo-exibicao";
+    
+    let rodada = 1;
+    container.className = "jogo-exibicao"; //Aplicando a classe a partir do css no html, e identificando qual seletor pelo nome delimitado pelas aspas.
     document.body.appendChild(container);
 
     novaRodada();
     
     function novaRodada() {
-        if (rodada >= 12) { //O jogo em tese, nunca iria acabar se não fosse por esta restrição.
+        if (rodada >= 4) { //O jogo em tese, nunca iria acabar se não fosse por esta restrição.
             novoJogo(); //A função pergunta se o jogador deseja finalizar o jogo ou não?
         }
 
@@ -34,8 +36,10 @@ function jogoExecutando() {
         const quantidadeQuadrados = 4 * (rodada + 1);
         const clique_ordem = [];
         let venceu_rodada = false; //Inicialmente será falso, não é possível ganhar se nunca jogou antes ou clicou na função anônima em: peca.onclick = () => {...}, linhas: 45 a 53.
+        const peca_grade = document.createElement("div");
 
-        for (let i = 0; i < quantidadeQuadrados; i++) {
+        let i = 0;
+        for (; i < quantidadeQuadrados; i++) {
             const peca = document.createElement("div");
             peca.className = "grade-jogo";
             peca.style.backgroundColor = corAleatoria();
@@ -51,11 +55,24 @@ function jogoExecutando() {
                     const parabens = document.createElement("p");
                     parabens.textContent = "Você venceu esta rodada!";
                     container.appendChild(parabens);
+                    return venceu_rodada;
                 }
-            };
+            };  
 
+            let onmouseover = true; //Variável para controlar o elemento DOM, o nome da variável é o mesmo que do elemento.
+            if (onmouseover === true){
+                //.onmouseover é uma propriedade DOM equivalente ao hover (a pseudoclasse)
+                peca_grade.onmouseover = () =>{ //A variável será usada aqui.
+                    if(onmouseover === true){
+                        onmouseover = false; //Alterando o valor da variável aqui dentro.
+                        intervalo();
+                    }
+                };
+            }
+
+            peca_grade.appendChild(peca)
             clique_ordem.push(peca);
-            container.appendChild(peca);
+            container.appendChild(peca_grade);
         }
 
         const ordem_para_usuario = clique_ordem[0].style.backgroundColor;
@@ -67,16 +84,20 @@ function jogoExecutando() {
         const temporizador_p = document.createElement("p");
         let tempoRestante = 8;
 
-        const intervalo = setInterval(() => {
-            temporizador_p.textContent = `Você tem 8 segundos para clicar na cor correta: ${tempoRestante}s`;
-            tempoRestante--;
+        function intervalo(){
+            
+            const intervalo_var = setInterval(() => { //Variável intervalo
+                temporizador_p.textContent = `Você tem 8 segundos para clicar na cor correta: ${tempoRestante}s`;
+                tempoRestante--;
 
-            if (tempoRestante < 0 && venceu_rodada === true) {
-                clearInterval(intervalo);
-                temporizador_p.textContent = "Tempo esgotado! \nObrigado por jogar";
-                finalizar();
-            }
-        }, 1000);
+                if (tempoRestante === 0 && venceu_r) {
+                    clearInterval(intervalo_var);
+                    temporizador_p.textContent = "Tempo esgotado! \nObrigado por jogar";
+                    finalizar(container, interruptor);
+                    return;
+                }
+            }, 1000);
+        }
         //Fim do temporizador
 
         container.appendChild(temporizador_p);
@@ -94,12 +115,12 @@ function jogoExecutando() {
         botaoSim.textContent = "Sim";
         botaoSim.style.backgroundColor = corAleatoria(); // cor aleatória no botão Sim
         
-        botaoSim.onclick = function () { finalizar_jogo = true; finalizar(); };
+        botaoSim.onclick = function () { finalizar(container, interruptor); };
 
         const botaoNao = document.createElement("button");
         botaoNao.textContent = "Não";
         botaoNao.onclick = () => {
-                if (venceu_rodada){
+            if (venceu_rodada){
                 rodada++; // O jogo termina a partir da 13° rodada.
                 novaRodada();
             }
@@ -110,16 +131,9 @@ function jogoExecutando() {
         container.appendChild(botoesContainer);
     }
 
-    function finalizar() {
-        if (finalizar){
-            container.remove();
-            return;
-        }
-    }
-
     function novoJogo(){
         const repetir = document.createElement("p");
-        repetir.textContent = "Deseja jogar novamente? "
+        repetir.textContent = "Deseja jogar novamente? ";
 
         const sim = document.createElement("button");
         sim.textContent = "Sim";
@@ -130,7 +144,7 @@ function jogoExecutando() {
         const nao = document.createElement("button");
         nao.textContent = "Nao";
         nao.onclick = () =>{
-            finalizar();
+            finalizar(container, interruptor);
         }
 
         container.append(repetir);
@@ -139,6 +153,19 @@ function jogoExecutando() {
 
     }
 
+    function finalizar(container, interruptor) {
+        console.log("Interruptor é recebido: ", interruptor);
+        fechar.textContent = "Fechar";
+        container.append(fechar);
+
+        fechar.onclick = () => {
+            container.remove();
+            const fechando = document.createElement("div");
+            return;
+        }
+    } 
+
+    
     function corAleatoria() {
         
         const cores = ["blue", "red", "pink", "gray", "yellow", "white", "brown"];
@@ -150,3 +177,6 @@ function jogoExecutando() {
     }
     
 }
+
+
+
